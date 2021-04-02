29b9a98e34a135bb953e3e8d97404a9d1fcfa514
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2021-04-01 21:04:10 -0500

    initial commit

---
+++ hello.js
@@ -0,0 +1,41 @@
+// var http = require("http");
+// var url = require("url"); // for working with the url of the node server webpage
+
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     var q = url.parse(req.url, true).query;
+//     var dates = q.year;
+//     res.end(dates);
+//   })
+//   .listen(8080);
+
+var http = require("http");
+var fs = require("fs"); // for working with files
+var url = require("url");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (filename == "./") {
+      filename = "./index";
+    }
+
+    filename = filename + ".html";
+
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 not fonund");
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      console.log("... Incoming Request: " + req.url); // how to create server console status messages
+      return res.end();
+    });
+  })
+  .listen(8080);
+
+console.log("Server listening");
